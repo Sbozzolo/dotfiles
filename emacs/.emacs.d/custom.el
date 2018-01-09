6f49a5ea57c8194509180152bc73a979723ab6e8
Gabriele Bozzola <sbozzolator@gmail.com>
2018-01-09 22:08:07 +0100

    Version 2.0.1.1
       *settings.org*
       - Added move-text to move lines with M-<up> and M-<down>
       - Rebound text-font-increase and decrease to use C-- as negative
         universal argumetn
       *zshrc_function*
       - Added .trash, now files are not deleted but moved to trash,
         but they are deleted if rm is invoked in the .trash folder

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,10 +7,10 @@
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(font-latex-script-display (quote ((raise -0.2) raise 0.2)) t)
+ '(font-latex-script-display (quote ((raise -0.2) raise 0.2)))
  '(package-selected-packages
    (quote
-    (recentf-mode winner-mode yahoo-weather fancy-battery spaceline-all-the-icons spaceline all-the-icons eterm-256color sh sh-mode langtool xterm-color comment-dwim-2 solarized-theme ibuffer-vc company-quickhelp diredfl pdf-tools ox-reveal mu4e-alert eimp markdown-mode easy-hugo ein theme-changer define-word company-auctex highlight-symbol rainbow-delimiters org-tree-slide paredit company-c-headers guess-language auto-package-update whole-line-or-region whitespace-cleanup-mode which-key use-package undo-tree systemd synonyms smex smart-mode-line-powerline-theme session org-bullets noflet multi-term magit ledger-mode iflipb htmlize guru-mode google-translate flycheck-pos-tip exwm expand-region engine-mode counsel company color-theme-sanityinc-solarized auctex)))
+    (move-text portage-navi recentf-mode winner-mode yahoo-weather fancy-battery spaceline-all-the-icons spaceline all-the-icons eterm-256color sh sh-mode langtool xterm-color comment-dwim-2 solarized-theme ibuffer-vc company-quickhelp diredfl pdf-tools ox-reveal mu4e-alert eimp markdown-mode easy-hugo ein theme-changer define-word company-auctex highlight-symbol rainbow-delimiters org-tree-slide paredit company-c-headers guess-language auto-package-update whole-line-or-region whitespace-cleanup-mode which-key use-package undo-tree systemd synonyms smex smart-mode-line-powerline-theme session org-bullets noflet multi-term magit ledger-mode iflipb htmlize guru-mode google-translate flycheck-pos-tip exwm expand-region engine-mode counsel company color-theme-sanityinc-solarized auctex)))
  '(safe-local-variable-values
    (quote
     ((TeX-command-extra-options . "-shell-escape")
@@ -19,5 +19,11 @@
      (reftex-default-bibliography "/home/sbozzolo/master_thesis/thesis/biblio.bib")
      (reftex-default-bibliography "biblio.bib")
      (eval ispell-change-dictionary "en_US"))))
- '(session-use-package t nil (session))
-)
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sml/charging ((t (:inherit sml/global :foreground "lawn green" :weight bold :height 1.1))))
+ '(sml/discharging ((t (:inherit sml/global :foreground "Red" :weight bold :height 1.1)))))
